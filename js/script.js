1e15048811c9f5727c39b9c488c5c325af9499a8
Dustin Coates <dustin@dcoates.com>
2016-12-27 17:41:46 +0100

    Auto lang switching

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
+// Please note: I know this is hacky. Please don't judge.
+
 var unselected = document.querySelector('#language-selector a:not(.selected)');
+var lang = (navigator.language || navigator.userLanguage).match(/fr/) ? 'fr' : 'en';
+
+if(lang === 'fr') {
+  var main = document.querySelector('.main');
+  main.classList.toggle('fr');
+  main.classList.toggle('en');
+
+  var langSelectors = document.getElementsByClassName('select-lang');
+  for (var i = 0; i < langSelectors.length; i++) {
+    langSelectors[i].classList.toggle('selected');
+  }
+
+  unselected = document.querySelector('#language-selector a:not(.selected)');
+}
+
 
 function clickEvent(e) {
   e.preventDefault();
